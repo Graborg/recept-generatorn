87b784f49c4a2f9710abb0358edffbc67ae6227c
Mikael Gråborg <mikael.graborg@iteam.se>
2020-02-10 19:15:49 +0100

    chore: fix random recipe page

--- lib/recept_generatorn_web/live/random_recipe_live.ex
+++ lib/recept_generatorn_web/live/random_recipe_live.ex
@@ -1,20 +1,8 @@
 defmodule ReceptGeneratornWeb.RandomRecipeLive do
   use Phoenix.LiveView
   alias ReceptGeneratorn.Recipe
-
   def mount(_session, socket) do
-    recipe = Recipe.get_random_recipe()
-    # case getRecipe() do
-    #   nil ->
-    #     conn
-    #     |> put_flash(:info, "You need to first create a recipe")
-    #     |> redirect( to: "/new")
-    #   recipe ->
-    #     conn
-    #     |> assign(:name, recipe.name)
-    #     |> render("random.html")
-    # end
-    {:ok, assign(socket, name: recipe.name)}
+    {:ok, assign(socket, :name, Recipe.get_random_recipe() |> Map.get(:name))}
   end
 
   def render(assigns) do
