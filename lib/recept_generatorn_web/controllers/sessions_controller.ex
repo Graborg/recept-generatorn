93a0b4c683d2d71cffd96c88e122289753d6472a
Mikael Gråborg <mikael.graborg@iteam.se>
2020-06-30 22:52:37 +0200

    feat: user login

--- lib/recept_generatorn_web/controllers/sessions_controller.ex
+++ lib/recept_generatorn_web/controllers/sessions_controller.ex
@@ -2,8 +2,15 @@ defmodule ReceptGeneratornWeb.SessionController do
   use ReceptGeneratornWeb, :controller
 
   def create(conn, %{"session" => %{"username" => username}}) do
+    user_id =
+      case ReceptGeneratorn.Accounts.get_user_by_username!(username) do
+        nil -> ReceptGeneratorn.Accounts.create_user(%{username: username})
+        user -> user
+      end
+      |> Map.get(:id)
+
     conn
-    |> put_session(:username, username)
+    |> put_session(:user_id, user_id)
     |> redirect(to: "/")
   end
 end
